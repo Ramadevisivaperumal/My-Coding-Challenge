d2ce59132385823a422d05c8eb7930a60736c2b0
Ramadevisivaperumal <113254790+Ramadevisivaperumal@users.noreply.github.com>
2023-06-22 12:56:20 +0530

    Add files via upload

---
+++ day 0 challenge.py
@@ -0,0 +1,26 @@
+#sum of two numbers
+num1=2;
+num2=4;
+num3=6;
+print(num1+num2);
+print(num1+num3);
+print(num3+num2);
+      
+
+
+#find radius of the circle
+print("enter radius value");
+r=int(input());
+pi=3.14;
+area=pi*r*r;
+print("area is",format(area,"0.2f"));
+
+#find even or not
+number=int(input("enter the number"));
+num=number%2;
+if(num==0):
+    print("yes");
+else:
+   print("no");
+    
+        
