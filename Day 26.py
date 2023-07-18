51e893997c6e2ba42165e656b319bdbd5b1ac02e
Ramadevisivaperumal <113254790+Ramadevisivaperumal@users.noreply.github.com>
2023-07-18 11:35:24 +0530

    Add files via upload

---
+++ Day 26.py
@@ -0,0 +1,86 @@
+'''
+Program 1 Output:
+* * * * *
+* * * * 
+* * * 
+* *  
+*
+'''
+
+n=int(input())
+num=n+1
+for i in range(n):
+    num=num-1
+    for j in range(1,num+1):
+        print("*",end='')
+    print()
+'''
+Program 2 Output:
+1
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+'''
+M=int(input())
+num=1
+for i in range(1,M+1):
+    num=num+1
+    for j in range(1,num):
+        print(i,end='')
+    print()
+
+
+
+'''
+ program 3 Output:
+1
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+'''
+N=int(input())
+num=1
+for i in range(N):
+    num=num+1
+    for j in range(1,num):
+        print(j,end='')
+    print()
+
+'''    
+program 4 Output:
+1 2 3 4 5
+1 2 3 4
+1 2 3 
+1 2  
+1 
+'''
+
+N=int(input())
+num=N+1
+for i in range(N):
+    num=num-1
+    for j in range(1,num+1):
+        print(j,end='')
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
