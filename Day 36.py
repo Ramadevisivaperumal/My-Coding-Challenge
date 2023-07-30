5008e9dc189b81d96b98a5920f5bad1fdf882d2d
Ramadevisivaperumal <113254790+Ramadevisivaperumal@users.noreply.github.com>
2023-07-30 17:14:51 +0530

    Add files via upload

---
+++ Day 36.py
@@ -0,0 +1,9 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:04:37) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:\Users\TWINS\AppData\Local\Programs\Python\Python39-32\Day 35.py =
+4
+24
+4
+5
+>>> 
